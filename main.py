3dbb76de6220d373a2861bedb160d489b5445c22
m4rsland1498 <jm4rsland@gmail.com>
2025-04-11 16:44:59 +0100

    Cursor select now fully functional. Correctly checks if player has found the square and prints to terminal as validation (temporary printing).

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ import pygame
 import random
 import time
 
-def initialCanvas(screen):
+def initialCanvas(screen, screenArray):
     pygame.draw.rect(screen, [0,0,0], (0,600,200,200))
     for i in range(8):
         for j in range(6):
-            r=random.randint(0,255)
-            g=random.randint(0,255)
-            b=random.randint(0,255)
-            pygame.draw.rect(screen, [r,g,b], (i*100, j*100, 100,100))
+            r=random.randint(0,80)
+            g=random.randint(120,255)
+            b=random.randint(0,120)
+            screenArray.append(([r,g,b], (i*100, j*100, 100,100)))
 
     pygame.display.flip()
 
@@ -19,7 +19,7 @@ def drawShapes(screen, width, height):
     r=random.randint(0,255)
     g=random.randint(0,255)
     b=random.randint(0,255)
-    points = []
+#    points = []
 #    numOfPoints = random.randint(3,5)
 #    for i in range(numOfPoints):
 #        x = random.randint(0,width)
@@ -43,14 +43,48 @@ def toFind(screen, width, height):
     sqSubsurface = pygame.Surface((length,length))
     sqSubsurface.blit(displaySurface, (0,0), sqRect)
 
-    screen.blit(sqSubsurface, (100-0.5*length,700-0.5*length))
-    pygame.display.flip()
+    #blitVals = [sqSubsurface, (100-0.5*length,700-0.5*length)]
+    blitVals.append(sqSubsurface)
+    blitVals.append((100-0.5*length,700-0.5*length))
+    global xToFind, yToFind, lengthToFind
+    xToFind = x
+    yToFind = y
+    lengthToFind = length
 
 def drawSelect(screen, startPos, currentPos):
+    # v1
     w = currentPos[0]-startPos[0]
+    if w < 1:
+        w = 1
     h = currentPos[1]-startPos[1]
-    pygame.draw.rect(screen, [0,0,255], (startPos[0], startPos[1], w, h))
+    if h < 1:
+        h = 1
+    #pygame.draw.rect(screen, [0,0,255], (startPos[0], startPos[1], w, h))
+    #pygame.display.flip()
+
+    # v2
+    selectBox = pygame.Surface((w, h), pygame.SRCALPHA)
+    selectBox.set_alpha(128)
+    selectBox.fill((50,200,255))
+    screen.blit(selectBox, (startPos[0], startPos[1]))
     pygame.display.flip()
+
+def redraw(screen, screenArray):
+    screen.fill("white")
+    pygame.draw.rect(screen, [0,0,0], (0,600,200,200))
+    for i in screenArray:
+        colour = i[0]
+        pos_s = i[1]
+        pygame.draw.rect(screen, colour, pos_s)
+    screen.blit(blitVals[0], blitVals[1])
+    pygame.display.flip()
+
+def checkSelection():
+    if (xToFind - 50) <= startPos[0] <= (xToFind + 50):
+        if (yToFind - 50) <= startPos[1] <= (yToFind + 50):
+            if (lengthToFind - 50) <= (currentPos[0] - startPos[0]) <= (lengthToFind + 50):
+                if (lengthToFind - 50) <= (currentPos[1] - startPos[1]) <= (lengthToFind + 50):
+                    print("Found!")
 
 def main():
     pygame.init()
@@ -61,16 +95,28 @@ def main():
 
     # virtual height
     height = 600
-    
-    initialCanvas(screen)
+
+    screenArray = []
+    initialCanvas(screen, screenArray)
+    for i in screenArray:
+        colour = i[0]
+        pos_s = i[1]
+        pygame.draw.rect(screen, colour, pos_s)
     #numOfShapes = 10
     #for i in range(numOfShapes):
     #    drawShapes(screen, width, height)
+    global blitVals
+    blitVals = []
     toFind(screen, width, height)
+    screen.blit(blitVals[0], blitVals[1])
+    pygame.display.flip()
 
     running = True
     isJustClicked = False
-    startPos, currentPos = pygame.mouse.get_pos()
+    global startPos, currentPos
+    startPos = pygame.mouse.get_pos()
+    currentPos = pygame.mouse.get_pos()
+    print(startPos)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -81,8 +127,13 @@ def main():
                 drawSelect(screen, startPos, startPos)
             elif pygame.mouse.get_pressed()[0]:
                 currentPos = pygame.mouse.get_pos()
+                redraw(screen, screenArray)
                 drawSelect(screen, startPos, currentPos)
-            else:
+            if not(pygame.mouse.get_pressed()[0]):
+                checkSelection()
+                startPos = pygame.mouse.get_pos()
+                currentPos = pygame.mouse.get_pos()
+                redraw(screen, screenArray)
                 isJustClicked = False
 
 
